dd303e4557a80e26aa38d98a8dff646c6a8de8f0
Your Name <you@example.com>
2023-02-27 15:02:05 +0700

    Bai 1 + 2

--- AP11/BT1+2.ts
+++ AP11/BT1+2.ts
@@ -8,15 +8,17 @@ class InsertionSort {
         let array = this.arr;
         let key;
         for (let i = 1; i < array.length; i++) {
+            console.log(`Lần kiểm tra số ${i}`);
             key = array[i]
             let j = i-1;
             while (j>=0 && array[j]>key){
                 array[j+1] = array[j];
-                j--
+                j--;
             }
             array[j+1] = key;
+            console.log(`Sau khi kiểm tra lần ${i}: ${array}`);
         }
-        console.log(array)
+        console.log("Sau khi hoàn thành: "+array)
     }
 }
 let arr = new InsertionSort([3,2,1,5,9,7,6]);
