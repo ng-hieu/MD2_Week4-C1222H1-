a386cb48f7468b2051d1b31c88a7646cd3f02e16
Your Name <you@example.com>
2023-02-27 14:48:55 +0700

    BT 1+2

---
+++ AP11/BT1+2.ts
@@ -0,0 +1,23 @@
+class InsertionSort {
+    arr:number[]
+
+    constructor(arr: number[]) {
+        this.arr = arr;
+    }
+    insertionSort(){
+        let array = this.arr;
+        let key;
+        for (let i = 1; i < array.length; i++) {
+            key = array[i]
+            let j = i-1;
+            while (j>=0 && array[j]>key){
+                array[j+1] = array[j];
+                j--
+            }
+            array[j+1] = key;
+        }
+        console.log(array)
+    }
+}
+let arr = new InsertionSort([3,2,1,5,9,7,6]);
+arr.insertionSort();
